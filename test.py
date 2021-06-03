e5e6510a4ea7bae1dcd08c8268d5c3d2e55d10d6
Alfadian Owen <alfafianowen.ao@gmail.com>
2021-06-03 09:13:45 +0700

    ketinggalan

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ image_of_c = face_recognition.load_image_file('./img/known/Chandra.jpeg')
 c_face_encoding = face_recognition.face_encodings(image_of_c)[0]
 
 
-#  Create arrays of encodings and names
 known_face_encodings = [
     c_face_encoding,
 
@@ -15,35 +14,27 @@ known_face_names = [
     "Chandra"
 ]
 
-# Load test image to find faces in
 test_image = face_recognition.load_image_file('./img/groups/d.jpeg')
 
-# Find faces in test image
 face_locations = face_recognition.face_locations(test_image)
 face_encodings = face_recognition.face_encodings(test_image, face_locations)
 
-# Convert to PIL format
 pil_image = Image.fromarray(test_image)
 
-# Create a ImageDraw instance
 draw = ImageDraw.Draw(pil_image)
 
-# Loop through faces in test image
 for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
     matches = face_recognition.compare_faces(
         known_face_encodings, face_encoding)
 
     name = "Unknown Person"
 
-    # If match
     if True in matches:
         first_match_index = matches.index(True)
         name = known_face_names[first_match_index]
 
-    # Draw box
     draw.rectangle(((left, top), (right, bottom)), outline=(255, 255, 0))
 
-    # Draw label
     text_width, text_height = draw.textsize(name)
     draw.rectangle(((left, bottom - text_height - 10), (right, bottom)),
                    fill=(255, 255, 0), outline=(255, 255, 0))
@@ -51,5 +42,6 @@ for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodin
 
 del draw
 
-# Display image
 pil_image.show()
+
+pil_image.save('identifys.jpg')
