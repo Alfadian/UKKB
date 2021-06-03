fbe4463aa3b3a52ad80c3c9a30989a1e18e0cbda
Alfadian Owen <alfafianowen.ao@gmail.com>
2021-06-03 09:11:33 +0700

    codingan baru

--- test.py
+++ test.py
@@ -1,55 +1,55 @@
 import face_recognition
-import os
-import cv2
-import streamlit as st
+from PIL import Image, ImageDraw
 
-KNOWN_FACES_DIR = "known_faces"
-UNKNOWN_FACES_DIR = "unknown_faces"
-TOL = 0.6
-FRAME_THICKNESS = 3
-FONT_THICKNESS = 2
-MODEL = "cnn"
+image_of_c = face_recognition.load_image_file('./img/known/Chandra.jpeg')
+c_face_encoding = face_recognition.face_encodings(image_of_c)[0]
 
-known_faces = []
-known_names = []
 
-for name in os.listdir(KNOWN_FACES_DIR):
-    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
-        image = face_recognition.load_file_image(f"{KNOWN_FACES_DIR}/{name}")
-        encoding = face_recognition.face_encoding(image)
-        known_faces.append(encoding)
-        known_names.append(name)
+#  Create arrays of encodings and names
+known_face_encodings = [
+    c_face_encoding,
 
-print("data sedang di proses")
-for filename in os.listdir(UNKNOWN_FACES_DIR):
-    print(filename)
-    image = face_recognition.load_file_image(f"{KNOWN_FACES_DIR}/{name}")
-    locations = face_recognition.face_locations(image, model=MODEL)
-    encodings = face_recognition.face_encodings(image, locations)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+]
 
-    for face_encoding, face_location in zip(encodings, locations):
-        result = face_recognition.compare_faces(
-            known_faces, face_encoding, TOL)
-        match = None
-        if True in result:
-            match = known_names[result.index(True)]
-            print(f"match found : {match}")
+known_face_names = [
+    "Chandra"
+]
 
-            top_left = (face_location[3], face_location[0])
-            bottom_right = (face_location[1], face_location[2])
+# Load test image to find faces in
+test_image = face_recognition.load_image_file('./img/groups/d.jpeg')
 
-            color = [0, 255, 0]
+# Find faces in test image
+face_locations = face_recognition.face_locations(test_image)
+face_encodings = face_recognition.face_encodings(test_image, face_locations)
 
-            cv2.rectangle(image, top_left, bottom_right,
-                          color, FRAME_THICKNESS)
+# Convert to PIL format
+pil_image = Image.fromarray(test_image)
 
-            top_left = (face_location[3], face_location[2])
-            bottom_right = (face_location[1], face_location[2]+22)
-            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-            cv2.putText(image, match, (face_location[3]+10, face_location[2]+15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+# Create a ImageDraw instance
+draw = ImageDraw.Draw(pil_image)
 
-    cv2.imshow(filename, image)
-    cv2.waitKey(10000)
-    # cv2.destroyWindow(filename)
+# Loop through faces in test image
+for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    matches = face_recognition.compare_faces(
+        known_face_encodings, face_encoding)
+
+    name = "Unknown Person"
+
+    # If match
+    if True in matches:
+        first_match_index = matches.index(True)
+        name = known_face_names[first_match_index]
+
+    # Draw box
+    draw.rectangle(((left, top), (right, bottom)), outline=(255, 255, 0))
+
+    # Draw label
+    text_width, text_height = draw.textsize(name)
+    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)),
+                   fill=(255, 255, 0), outline=(255, 255, 0))
+    draw.text((left + 6, bottom - text_height - 5), name, fill=(0, 0, 0))
+
+del draw
+
+# Display image
+pil_image.show()
